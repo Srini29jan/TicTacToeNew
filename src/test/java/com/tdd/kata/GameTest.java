750d9180a791ca4db03ee9e2987cfbef30992b6f
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-08-22 15:22:09 +0530

    extracted constant

--- src/test/java/com/tdd/kata/GameTest.java
+++ src/test/java/com/tdd/kata/GameTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertThat;
 public class GameTest {
 
     private static final int POSITION_ONE = 1;
+    private static final int POSITION_TWO = 2;
     private static final int POSITION_ZERO = 0;
     private static final char PLAYER_O = 'O';
     private static final char PLAYER_X = 'X';
@@ -40,7 +41,7 @@ public class GameTest {
         game.playAt(POSITION_ONE, POSITION_ZERO);
         game.playAt(POSITION_ZERO, POSITION_ONE);
         game.playAt(POSITION_ONE, POSITION_ONE);
-        game.playAt(POSITION_ZERO, 2);
+        game.playAt(POSITION_ZERO, POSITION_TWO);
 
         assertThat(game.getWinner(), Is.is(PLAYER_X));
     }
