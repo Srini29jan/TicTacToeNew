9a570a2a318804e32840b1512f704b1441b6ccad
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-08-22 21:58:49 +0530

    verified if O is winner if O fills second column

--- src/test/java/com/tdd/kata/GameTest.java
+++ src/test/java/com/tdd/kata/GameTest.java
@@ -152,4 +152,16 @@ public class GameTest {
 
         assertThat(game.getWinner(), is(PLAYER_X));
     }
+
+    @Test
+    public void getWinnerShouldReturnOIfOFillsSecondColumn() {
+        game.playAt(POSITION_TWO, POSITION_ZERO);
+        game.playAt(POSITION_ZERO, POSITION_ONE);
+        game.playAt(POSITION_ZERO, POSITION_TWO);
+        game.playAt(POSITION_ONE, POSITION_ONE);
+        game.playAt(POSITION_ZERO, POSITION_ZERO);
+        game.playAt(POSITION_TWO, POSITION_ONE);
+
+        assertThat(game.getWinner(), is(PLAYER_O));
+    }
 }
