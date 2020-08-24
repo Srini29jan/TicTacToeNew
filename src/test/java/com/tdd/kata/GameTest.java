4670231760648926c46348cc19759f04151a2fa8
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-08-24 23:58:27 +0530

    verified if there is no winner if the board is full and there is no same symbol in a row or column or diagonal

--- src/test/java/com/tdd/kata/GameTest.java
+++ src/test/java/com/tdd/kata/GameTest.java
@@ -233,4 +233,19 @@ public class GameTest {
 
         assertThat(game.getWinner(), is(PLAYER_O));
     }
+
+    @Test
+    public void getWinnerShouldReturnNullIfAllCellsAreFilledAndNeitherPlayerWon() {
+        game.playAt(POSITION_ZERO, POSITION_ZERO);
+        game.playAt(POSITION_ONE, POSITION_ONE);
+        game.playAt(POSITION_TWO, POSITION_ZERO);
+        game.playAt(POSITION_ONE, POSITION_ZERO);
+        game.playAt(POSITION_ONE, POSITION_TWO);
+        game.playAt(POSITION_ZERO, POSITION_ONE);
+        game.playAt(POSITION_TWO, POSITION_ONE);
+        game.playAt(POSITION_TWO, POSITION_TWO);
+        game.playAt(POSITION_ZERO, POSITION_TWO);
+
+        assertThat(game.getWinner(), is('\0'));
+    }
 }
