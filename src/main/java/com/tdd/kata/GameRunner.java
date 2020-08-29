7cc1e3eaca55339a599e8fab228d7d5a7f0234da
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-08-29 17:39:18 +0530

    modified access for constants and rearranged constants

--- src/main/java/com/tdd/kata/GameRunner.java
+++ src/main/java/com/tdd/kata/GameRunner.java
@@ -5,10 +5,10 @@ import java.util.logging.Logger;
 
 public class GameRunner {
     private static final Logger logger = Logger.getLogger(GameRunner.class.getName());
+    private static final String FIRST_ROW = "1 2 3";
     private static final String POSITION_INFORMATION_MESSAGE = "Position of each box is represented by a number as below";
+    private static final String SECOND_ROW = "4 5 6";
     private static final String WELCOME_MESSAGE = "Welcome to TicTacToe 3X3";
-    public static final String FIRST_ROW = "1 2 3";
-    public static final String SECOND_ROW = "4 5 6";
 
     public void play() {
         print(WELCOME_MESSAGE);
