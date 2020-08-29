b0d11d54bffcc9b25a843260dd3d0683aadc232f
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-08-29 17:09:51 +0530

    extracted constants

--- src/main/java/com/tdd/kata/GameRunner.java
+++ src/main/java/com/tdd/kata/GameRunner.java
@@ -5,10 +5,12 @@ import java.util.logging.Logger;
 
 public class GameRunner {
     private static final Logger logger = Logger.getLogger(GameRunner.class.getName());
+    private static final String POSITION_INFORMATION_MESSAGE = "Position of each box is represented by a number as below";
+    private static final String WELCOME_MESSAGE = "Welcome to TicTacToe 3X3";
 
     public void play() {
-        print("Welcome to TicTacToe 3X3");
-        print("Position of each box is represented by a number as below");
+        print(WELCOME_MESSAGE);
+        print(POSITION_INFORMATION_MESSAGE);
     }
 
     protected void print(String message) {
